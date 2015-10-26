64a71ee6cb80e2e0e6912f49ba761d1c42335c5d
Grahame Bowland <grahame@angrygoats.net>
2015-10-26 10:23:04 +0800

    dont zero out PASSPHRASE

--- automongobackup.sh
+++ automongobackup.sh
@@ -266,7 +266,7 @@ VER=0.10                                          # Version Number
 LOGFILE=$BACKUPDIR/$DBHOST-`date +%H%M`.log       # Logfile Name
 LOGERR=$BACKUPDIR/ERRORS_$DBHOST-`date +%H%M`.log # Logfile Name
 BACKUPFILES=""
-PASSPHRASE=""                                      # set to something for encryption
+#PASSPHRASE=""                                      # set to something for encryption
 #OPT=""                                            # OPT string for use with mongodump
 
 # Do we need to use a username/password?
